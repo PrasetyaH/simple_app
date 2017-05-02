6c7ef50798c85b173186e6bcac3fd5954d625617
PrasetyaH <herwinprasetya@gmail.com>
2017-05-03 01:00:18 +0700

    update main.js
    to display address

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ var person = require ('./object/Person');
 var herwin = new person();
 herwin.setName('Herwin Prasetya');
 var nama = herwin.getName();
+var address =  herwin.address;
 console.log('My Name is : '+nama);
+console.log('My Address is : '+address);
